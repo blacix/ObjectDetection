78aa12118c460b301451d54b04e720a37da35770
Laszlo Bartok <blacix@hotmail.com>
2020-10-31 12:26:09 +0100

    config paths w/ os.path.join

--- config.py
+++ config.py
@@ -1,38 +1,38 @@
+import os
+
 CAMERA_ID = 1
 USE_CALIBRATION = False
+USE_GPU = True
 
 MODEL_NAME = "ssd_mobilenet_v2_320x320_coco17_tpu-8"
 # MODEL_NAME = "ssd_resnet50_v1_fpn_640x640_coco17_tpu-8"
 
-USE_GPU = True
+TF_MODELS_REPOSITORY_DIR = "tf_models_repository"
+TRAINING_DIR = os.path.join("workspace", "training_demo")
+MODEL_DIR = os.path.join(TRAINING_DIR, "models", MODEL_NAME)
+PRE_TRAINED_MODEL_DIR = os.path.join(TRAINING_DIR, "pre-trained-models", MODEL_NAME)
+EXPORT_DIR = os.path.join(TRAINING_DIR, "exported-models", MODEL_NAME)
 
-TF_MODELS_REPOSITORY_DIR = "tf_models_repository/"
-TRAINING_DIR = "workspace/training_demo/"
-MODEL_DIR = TRAINING_DIR + "models/" + MODEL_NAME + "/"
-PRE_TRAINED_MODEL_DIR = TRAINING_DIR + "pre-trained-models/" + MODEL_NAME + "/"
-ANNOTATIONS_DIR = TRAINING_DIR + "annotations/"
-EXPORT_DIR = TRAINING_DIR + "exported-models/" + MODEL_NAME + "/"
+ANNOTATIONS_DIR = os.path.join(TRAINING_DIR, "annotations")
+LABEL_FILE = os.path.join(ANNOTATIONS_DIR, "label_map.pbtxt")
+TEST_RECORD_FILE = os.path.join(ANNOTATIONS_DIR, "test.record")
+TRAIN_RECORD_FILE = os.path.join(ANNOTATIONS_DIR, "train.record")
 
-LABEL_FILE = ANNOTATIONS_DIR + "label_map.pbtxt"
-
-PATH_TO_SAVED_MODEL = EXPORT_DIR + "saved_model/"
-PATH_TO_SAVED_MODEL = PRE_TRAINED_MODEL_DIR + "saved_model/"
-
+# for running
+PATH_TO_SAVED_MODEL = os.path.join(EXPORT_DIR, "saved_model")
+PATH_TO_SAVED_MODEL = os.path.join(PRE_TRAINED_MODEL_DIR, "saved_model")
 PATH_TO_LABELS = LABEL_FILE
-PATH_TO_LABELS = TF_MODELS_REPOSITORY_DIR + "/research/object_detection/data/mscoco_label_map.pbtxt"
+PATH_TO_LABELS = os.path.join(TF_MODELS_REPOSITORY_DIR, "research", "object_detection", "data", "mscoco_label_map.pbtxt")
 
 
-PRE_TRAINED_CHECKPOINT = PRE_TRAINED_MODEL_DIR + "checkpoint/"
-PIPELINE_CONFIG = MODEL_DIR + "pipeline.config"
+# PRE_TRAINED_CHECKPOINT = os.path.join(PRE_TRAINED_MODEL_DIR, "checkpoint")
+PIPELINE_CONFIG = os.path.join(MODEL_DIR, "pipeline.config")
 
-OBJ_DETECTION_SCRIPTS_DIR = TF_MODELS_REPOSITORY_DIR + "research/object_detection/"
-TRAIN_SCRIPT = OBJ_DETECTION_SCRIPTS_DIR + "model_main_tf2.py"
-EXPORT_SCRIPT = OBJ_DETECTION_SCRIPTS_DIR + "exporter_main_v2.py"
 
-TEST_RECORD_FILE = ANNOTATIONS_DIR + "test.record"
-TRAIN_RECORD_FILE = ANNOTATIONS_DIR + "train.record"
+OBJ_DETECTION_SCRIPTS_DIR = os.path.join(TF_MODELS_REPOSITORY_DIR, "research", "object_detection")
+TRAIN_SCRIPT = os.path.join(OBJ_DETECTION_SCRIPTS_DIR, "model_main_tf2.py")
+EXPORT_SCRIPT = os.path.join(OBJ_DETECTION_SCRIPTS_DIR, "exporter_main_v2.py")
 
-IMAGES_DIR = "workspace/training_demo/images/"
-TEST_IMAGES_DIR = IMAGES_DIR + "test/"
-TRAIN_IMAGES_DIR = IMAGES_DIR + "train/"
-
+IMAGES_DIR = os.path.join("workspace", "training_demo", "images")
+TEST_IMAGES_DIR = os.path.join(IMAGES_DIR, "test")
+TRAIN_IMAGES_DIR = os.path.join(IMAGES_DIR, "train")
