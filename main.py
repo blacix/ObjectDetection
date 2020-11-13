0074db57856897c6e92ba0e630165945b909d797
Laszlo Bartok <blacix@hotmail.com>
2020-11-13 16:19:07 +0100

    mostion detector added to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import camera_calibration as calib
 import config
 from object_detector import ObjectDetector
 from marker_detector import MarkerDetector
+from motion_detector import MotionDetector
 
 
 def main():
@@ -11,14 +12,17 @@ def main():
 
     object_detector = ObjectDetector()
     marker_detector = MarkerDetector()
+    motion_detector = MotionDetector()
 
     while True:
         ret, image = cap.read()
         if not ret:
             continue
 
+        image = motion_detector.process(image)
         image = object_detector.detect_objects(image)
         image, _, _ = marker_detector.detect_markers(image)
+
         cv.imshow('objects', image)
         if cv.waitKey(100) == ord('q'):
             break
