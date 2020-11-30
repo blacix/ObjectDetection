3597d004f169e789b84732e583910648e8d92111
László Bartók <blacix@hotmail.com>
2020-11-30 20:20:50 +0100

    threading stuff

--- image_processing.py
+++ image_processing.py
@@ -1,26 +1,38 @@
 import cv2 as cv
-
-import camera_calibration as calib
-import config
-from object_detector import ObjectDetector
+from camera_calibration import UndistortedVideoCapture
+# from object_detector import ObjectDetector
 from marker_detector import MarkerDetector
 from motion_detector import MotionDetector
+from threading import Thread
+import threading
+import time
 
 
 class ImageProcessor:
     def __init__(self, camera_id):
         self.marker_detector = MarkerDetector()
         self.motion_detector = MotionDetector()
-        self.object_detector = ObjectDetector()
+        # self.object_detector = ObjectDetector()
         self.camera_id = camera_id
-        self.cap = calib.UndistortedVideoCapture(self.camera_id)
+        # self.cap = UndistortedVideoCapture(self.camera_id)
+        # self.thread = Thread(target=self.loop)
+        # self.thread.start()
 
-    def process_image(self):
-        ret, image = self.cap.read()
+    # def loop(self):
+    #     while True:
+    #         print(threading.currentThread().ident)
+    #         ret, image = self.cap.read()
+    #         image = self.process_image(image)
+    #         time.sleep(0.01)
+    #         cv.imshow('camera {}'.format(self.camera_id), image)
+    #         if cv.waitKey(100) == ord('q'):
+    #             break
+
+    def process_image(self, image):
         # if not ret:
         #     return image
 
-        # image = self.motion_detector.process(image)
-        image = self.object_detector.detect_objects(image)
+        # image = self.object_detector.detect_objects(image)
+        image = self.motion_detector.process(image)
         image, _, _ = self.marker_detector.detect_markers(image)
         return image
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import cv2 as cv
 from image_processing import ImageProcessor
+from camera_calibration import UndistortedVideoCapture
+import threading
+cap0 = UndistortedVideoCapture(0)
+cap2 = UndistortedVideoCapture(2)
+image_processor0 = ImageProcessor(0)
+image_processor2 = ImageProcessor(2)
 
 
 def main():
-    # image_processor1 = ImageProcessor(0)
-    image_processor2 = ImageProcessor(2)
     while True:
-        # image1 = image_processor1.process_image()
-        image2 = image_processor2.process_image()
+        ret, image0 = cap0.read()
+        ret, image2 = cap2.read()
 
-        # cv.imshow('camera 0', image1)
+        image0 = image_processor0.process_image(image0)
+        image2 = image_processor2.process_image(image2)
+        cv.imshow('camera 0', image0)
         cv.imshow('camera 2', image2)
 
-        if cv.waitKey(100) == ord('q'):
+        if cv.waitKey(10) == ord('q'):
             break
 
     cv.destroyAllWindows()
