dca8a074ce609fd9a71119bd8a918e94ad316578
László Bartók <blacix@hotmail.com>
2020-12-15 17:51:48 +0100

    playing with asyncio

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import random
 import cv2 as cv
 from image_processing import ImageProcessor
 from camera_calibration import UndistortedVideoCapture
 import threading
-from threading import Thread
+import asyncio
 import time
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
@@ -74,5 +75,36 @@ def process(image_processor, cap):
     return image
 
 
+strings = ['a', 'b', 'c']
+
+
+async def async_func(string):
+    sleep_time = random.randint(0, 3)
+    print(f'async_func sleeps {sleep_time}')
+    await asyncio.sleep(sleep_time)
+    return string
+
+
+async def async_main():
+    # t1 = asyncio.create_task(async_func('x'))
+    # t2 = asyncio.create_task(async_func2('x2'))
+    #
+    # x1 = await t1
+    # x2 = await t2
+    #
+    # print(f'{x1} {x2} ')
+    # tasks = [asyncio.create_task(async_func(string)) for string in strings]
+    # for t in tasks:
+    #     res = await t
+    #     print(res)
+
+    coroutines = [async_func(string) for string in strings]
+    done, pending = await asyncio.wait(coroutines)
+    for coroutine in done:
+        print(coroutine.result())
+
+
 if __name__ == '__main__':
-    main()
+    asyncio.run(async_main())
+    # main()
+
