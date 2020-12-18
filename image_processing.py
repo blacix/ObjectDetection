97156f30d3ee469bea098d7dd306cced60c05efb
László Bartók <blacix@hotmail.com>
2020-12-18 19:45:07 +0100

    misc

--- image_processing.py
+++ image_processing.py
@@ -24,32 +24,35 @@ class ImageProcessor:
         return self.image
 
     def process_image_parallel(self, image):
-        # TODO
-        # motion_future = self.executor.submit(self.motion_detector.process, image)
-        # marker_future = self.executor.submit(self.marker_detector.process, image)
+        ret_image = np.zeros(image.shape, np.uint8)
 
-        # print(f'process_image_async {threading.currentThread().ident}')
-        # TODO all must have the same interface: .process(image)
-        arr = [self.motion_detector, self.marker_detector]
-        ret_image = image.copy()
-        future_to_processors = \
-            {self.executor.submit(p.process, image): p for p in arr}
-        for future in concurrent.futures.as_completed(future_to_processors):
-            p = future_to_processors[future]
-            try:
-                processed_image, _ = future.result()
-            except Exception as exc:
-                print(f"exception: {exc}")
-            else:
-                # image processors return images that contains only the "highlights" of ROI.
-                # simple addition changes colour, but fast
-                ret_image = cv.add(ret_image, processed_image)
+        motion_future = self.executor.submit(self.motion_detector.process, image)
+        marker_future = self.executor.submit(self.marker_detector.process, image)
 
-                # bitwise stuff is slow
-                # image_empty = np.zeros(image.shape, np.uint8)
-                # img_gray = cv.cvtColor(processed_image, cv.COLOR_BGR2GRAY)
-                # ret, mask = cv.threshold(img_gray, 10, 255, cv.THRESH_BINARY)
-                # ret_image = cv.bitwise_and(ret_image, image_empty, ret_image, mask=mask)
-                # ret_image = cv.bitwise_or(processed_image, ret_image, ret_image, mask=mask)
+        try:
+            motion_image, motion_detected = motion_future.result()
+            marker_image, ids = marker_future.result()
+        except Exception as exc:
+            print(f"exception: {exc}")
+        else:
+            ret_image = self.decorate_image(image, marker_image)
+            ret_image = self.decorate_image(ret_image, motion_image)
+
+        return ret_image
+
+    @staticmethod
+    def decorate_image(image, decoration_image):
+        # image processors return images that contains only the "highlights" of ROI.
+        # simple addition changes colour, but fast
+        ret_image = cv.add(image, decoration_image)
+
+        # # bitwise stuff is slow
+        # # TODO
+        # ret_image = image.copy()
+        # image_empty = np.zeros(image.shape, np.uint8)
+        # img_gray = cv.cvtColor(decoration_image, cv.COLOR_BGR2GRAY)
+        # ret, decoration_mask = cv.threshold(img_gray, 10, 255, cv.THRESH_BINARY)
+        # cv.bitwise_and(image, image_empty, ret_image, mask=decoration_mask)
+        # cv.bitwise_or(decoration_image, ret_image, ret_image, mask=decoration_mask)
 
         return ret_image
